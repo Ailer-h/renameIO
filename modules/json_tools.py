7bf067a4b1c57029a4686b07b5ed2646ed5613de
Ailer-h <henriqueailer2@gmail.com>
2025-09-07 12:42:10 -0300

    Added doc strings to a bunch of functions.

--- modules/json_tools.py
+++ modules/json_tools.py
@@ -1,7 +1,10 @@
 import json
 
-# Accesses a json file and returns it as a dict
 def get_dict(filepath: str) -> dict:
+    '''
+    Accesses a JSON file and returns it as a dictionary
+    '''
+
     try:
         with open(filepath, 'r') as json_file:
             data = json.load(json_file)
@@ -11,6 +14,9 @@ def get_dict(filepath: str) -> dict:
         return {}
     
 def update_json(dictionary: dict, json_path: str) -> None:
+    '''
+    Updates an existing JSON file
+    '''
     try:
         with open(json_path, 'w') as json_file:
             json.dump(dictionary, json_file, indent=4)
--- modules/logger.py
+++ modules/logger.py
@@ -1,13 +1,19 @@
 from colorama import Fore
 
 def log(log_id: str, msg: str, id_colour: str | None = None):
+    '''
+    Generic logging function. Used as building block to the
+    specialised ones.
+    '''
     if id_colour == None:
         id_colour = Fore.WHITE
     
     print(f"{id_colour}[{log_id}]{Fore.WHITE} {msg}")
 
 def log_error(message: str, log_id: str = "Error"):
+    '''Logs an error to the command line'''
     log(log_id, message, id_colour=Fore.RED)
 
 def log_info(message: str, log_id: str = "RenameIO"):
+    '''Logs generic information to the command line'''
     log(log_id, message)
